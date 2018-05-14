0204038242cf48bf837b01807378f7287cc57ed3
Jacek Gzel <jacek.gzel@gmail.com>
2018-05-14 19:25:06 +0200

    Decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
 
 
 ### features_train and features_test are the features for the training
@@ -19,13 +20,10 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = tree.DecisionTreeClassifier(min_samples_split=50)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print('Accurancy is: ', clf.score(features_test, labels_test))
