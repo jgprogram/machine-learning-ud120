3f3953a16724efdcbe5e506b1d050644850b5992
Jacek Gzel <jacek.gzel@gmail.com>
2018-06-11 19:40:35 +0200

    Regression

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,32 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+poi_names_file = open('../final_project/poi_names.txt', 'r')
 
 
+poi_in_file = list()
+poi_cnt = 0
+
+#for line in poi_names_file:
+    #if line.startswith('(y)'):
+#    poi_in_file.append(line[4:].upper().replace(', ', ' ').strip())
+
+# for person in enron_data:
+#    print enron_data[person]['email_address']
+#     for poi in poi_in_file:
+#         print poi
+#         if person.startswith(poi):
+#             poi_cnt += 1
+
+print('Folk with quantified salary: ', len([p for p in enron_data if enron_data[p]['salary'] != 'NaN']))
+print('Known email: ', len([p for p in enron_data if enron_data[p]['email_address'] != 'NaN']))
+
+print('Percentage of folks without total payments: {0}%'.format(len([p for p in enron_data if enron_data[p]['total_payments'] == 'NaN']) * 100 / len(enron_data)))
+print('Percentage of POIs without total payments: {0}%'.format(len([p for p in enron_data if enron_data[p]['poi'] and enron_data[p]['total_payments'] == 'NaN']) * 100 / len(enron_data)))
+print('People in dataset: {0}'.format(len(enron_data)))
+print('People without total payment: {0}'.format(len([p for p in enron_data if enron_data[p]['total_payments'] == 'NaN'])))
+print('Number of POIs: {0}'.format(len([p for p in enron_data if enron_data[p]['poi']])))
+
+# print(enron_data['SKILLING JEFFREY K']['total_payments'])
+# print(enron_data['SKILLING JEFFREY K']['total_payments'])
+# print(enron_data['LAY KENNETH S']['total_payments'])
--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,7 +37,14 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+
+print "R: " +  str(reg.score(feature_test, target_test))
+print "Slope: " + str(reg.coef_)
+print "Intercept: " + str(reg.intercept_)
 
 
 
@@ -67,4 +74,10 @@ except NameError:
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
+
+plt.xlabel(features_list[1])
+reg.fit(feature_test, target_test)
+print "New slope: ", reg.coef_
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
 plt.show()
